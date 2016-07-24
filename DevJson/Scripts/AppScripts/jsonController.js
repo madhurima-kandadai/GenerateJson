ec21fd95d8466dc20176ba1497606fdf32c288df
Madhurima <madhurima.kandadai@gmail.com>
2016-07-24 06:39:31 +0530

    Changed complete code - Implemented Add - Edit and Delete are pending.

--- DevJson/Scripts/AppScripts/jsonController.js
+++ DevJson/Scripts/AppScripts/jsonController.js
@@ -4,16 +4,11 @@
     $scope.swaggerEditorJson = [];
     $scope.models = [];
     $scope.tableViewModels = [];
-
-    $scope.modelTypes = [{
-        value: 'object',
-        name: 'Object'
-    }, {
-        value: 'array',
-        name: 'List',
-        type: 'primary'
-    }];
-
+    $scope.modelNames = [];
+    $scope.addProperty = {
+        required: false,
+        list: false
+    };
     $scope.dataTypes = [{
         value: 'int',
         name: 'Integer',
@@ -28,95 +23,95 @@
         type: 'primary'
     }];
 
-    $scope.SwaggerJsonGeneration = function () {
-        var index = 0;
-        $scope.swaggerEditorJson = [];
-        angular.forEach($scope.models, function (model) {
-            index = $scope.swaggerEditorJson.length;
-            $scope.swaggerEditorJson.push({
-                [model.model]: {
-                    "type": "object",
-                    "required": [],
-                    "properties": []
-                }
-            });
-            angular.forEach(model.properties, function (property) {
-                if (property.dataTypeType == 'primary') {
-                    var propertyscript = '$scope.swaggerEditorJson[index].' + model.model + '.properties.push({ [property.name]: {  "type": property.type,"items" : { "type": property.dataType }} });';
-                    eval(propertyscript);
-                } else if (property.dataTypeType == 'secondary') {
-                    var propertyscript = '$scope.swaggerEditorJson[index].' + model.model + '.properties.push({ [property.name]: { "type": property.type, "items" : { "$ref": property.dataType }} });';
-                    eval(propertyscript);
-                }
-
-                if (property.required === true) {
-                    var requiredScript = '$scope.swaggerEditorJson[index].' + model.model + '.required.push(property.name);';
-                    eval(requiredScript);
-                }
-            });
+    //Add Model 
+    $scope.AddModel = function () {
+        var exists = false;
+        angular.forEach($scope.models, function (obj) {
+            if (Object.keys(obj) == $scope.model) {
+                exists = true;
+            }
         });
-        console.log(JSON.stringify($scope.swaggerEditorJson, null, 2));
-        $scope.swaggerEditorJson = JSON.stringify($scope.swaggerEditorJson, null, 2);
-    };
-
-    $scope.GetModel = function (model) {
-        $scope.tableViewModels = [];
-        var index = $scope.models.findIndex(x => x.model.toLowerCase() == model.model.toLowerCase());
-        $scope.tableViewModels.push($scope.models[index]);
-    };
-
-    $scope.DeleteProperty = function (modelname, hashkey, propertyName) {
-        $scope.tableViewModels = [];
-        var index = $scope.models.findIndex(x => x.model.toLowerCase() == modelname.toLowerCase());
-        $scope.models[index].properties = jQuery.grep($scope.models[index].properties, function (obj) {
-            return obj.name != propertyName;
-        });
-        $scope.tableViewModels.push($scope.models[index]);
-    };
-
-    $scope.AddPropertyToModel = function () {
-        var d = new Date();
-        if (JSON.stringify($scope.models).indexOf(JSON.stringify($scope.modelName)) == -1) {
+        if (exists) {
+            alert('Duplicate Model Name');
+        }
+        else {
+            $scope.models.push({
+    [$scope.model]: {
+        "type": "object",
+        "required": [],
+        "properties": []
+    }
+            });
             $scope.dataTypes.push({
-                value: "#/definitions/" + $scope.modelName,
-                name: $scope.modelName,
+                value: '#/definitions/' + $scope.model,
+                name: $scope.model,
                 type: 'secondary'
             });
-            $scope.models.push({
-                "modelId": d.getMilliseconds(),
-                "model": $scope.modelName,
-                "properties": [{
-                    'propertyId': d.getMilliseconds(),
-                    "name": $scope.propertyName,
-                    "type": $scope.propertyType == true ? 'list' : 'object',
-                    "dataTypeType": jQuery.parseJSON($scope.dataType).type,
-                    "dataType": jQuery.parseJSON($scope.dataType).value,
-                    "required": $scope.required == null ? false : $scope.required
-                }]
-            });
-        } else {
-            var index = $scope.models.findIndex(x => x.model.toLowerCase() == $scope.modelName.toLowerCase());
-            if (index != -1) {
-                var propIndex = $scope.models[index].properties.findIndex(x => x.name.toLowerCase() == $scope.propertyName.toLowerCase());
-                if (propIndex == -1) {
-                    $scope.models[index].properties.push({
-                        'propertyId': d.getMilliseconds(),
-                        "name": $scope.propertyName,
-                        "type": $scope.propertyType == true ? 'list' : 'object',
-                        "dataTypeType": jQuery.parseJSON($scope.dataType).type,
-                        "dataType": jQuery.parseJSON($scope.dataType).value,
-                        "required": $scope.required == null ? false : $scope.required
-                    });
-                } else {
-                    alert("Duplicate property !~!!");
-                }
+            $scope.modelNames.push($scope.model);
+        }
+        $scope.model = '';
+    };
+
+    // Add property to model
+    $scope.AddPropertyToModel = function (property) {
+        debugger;
+        var dataTypeType = $scope.GetTypeOfDataType(property.dataType);
+        var index = $scope.models.findIndex(x => Object.keys(x).toLocaleString() == property.propertyModelName.toLocaleString());
+        var key = Object.keys($scope.models[index]);
+        if (property.required === true) {
+            $scope.models[index][key].required.push(property.propertyName);
+        }
+        if (property.list == false) {
+            if (dataTypeType == 'primary') {
+                $scope.models[index][key].properties.push({
+                    [property.propertyName]: {
+                        "type": property.dataType
+                    }
+                });
+            }
+            else {
+                $scope.models[index][key].properties.push({
+                    [property.propertyName]: {
+                        "$ref": property.dataType
+                    }
+                });
             }
         }
-        $scope.tableViewModels = $scope.models;
-        $scope.propertyName = '';
-        $scope.dataType = "";
-        $scope.propertyType = '';
-        $scope.required = null;
+        else {
+            if (dataTypeType == 'primary') {
+                $scope.models[index][key].properties.push({
+                    [property.propertyName]: {
+                        "type": "array",
+                        "items": {
+                            "type": property.dataType
+                        }
+                    }
+                });
+            }
+            else {
+                $scope.models[index][key].properties.push({
+                    [property.propertyName]: {
+                        "type": "array",
+                        "$ref": {
+                            "type": property.dataType
+                        }
+                    }
+                });
+            }
+        }
+
+        $scope.tableViewModels = JSON.stringify($scope.models, null, 2);
+        console.log($scope.tableViewModels);
+        $scope.addProperty.propertyModelName = "";
+        $scope.addProperty.propertyName = null;
+        $scope.addProperty.dataType = "";
+        $scope.addProperty.list = false;
+        $scope.addProperty.required = false;
+    }
+
+    $scope.GetTypeOfDataType = function (dataType) {
+        var index = $scope.dataTypes.findIndex(x => x.value == dataType);
+        return $scope.dataTypes[index].type;
     };
 
     $scope.EditProperty = function (model, property, hashkey) {
@@ -136,13 +131,4 @@
             closeByEscape: true
         });
     }
-
-    $scope.closengDialog = function () {
-        ngDialog.close();
-    }
-
-    $scope.updateProperty = function (editModel) {
-        //Object {modelName: "Emp", propertyName: "Id2", dataType: "int", propertyType: false, required: true}
-
-    };
 });
